db33681408d61d042c226ddd9f250b3d1d3a5d52
jinjinchen_microsoft <jinjinchen@microsoft.com>
2026-03-26 03:06:52 -0700

    max_length                   8192

--- QwenFinetune/infer_new_data.sh
+++ QwenFinetune/infer_new_data.sh
@@ -73,7 +73,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 /home/aiscuser/.conda/envs/vllm_infer/bin/python3.10 -m swift.cli.infer \
     --model                        "${MERGED_MODEL_PATH}" \
     --val_dataset                  "${INFER_INPUT}" \
-    --max_length                   16384 \
+    --max_length                   8192 \
     --infer_backend                vllm \
     --max_batch_size               32 \
     --vllm_tensor_parallel_size    8 \
